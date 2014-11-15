6914ea7a72fd45f0a4afd1b88592609476b26964
Alex Ignatenko <alex@getbase.com>
2014-11-15 13:55:34 +0100

    Populating shapes on user's touches

--- Chalk/Whiteboard/WhiteboardView.swift
+++ Chalk/Whiteboard/WhiteboardView.swift
@@ -10,18 +10,36 @@ import UIKit
 import CoreGraphics
 
 protocol WhiteboardViewDelegate {
-    func numberOfShapes() -> Int
-    func shapeAt(index: Int) -> Shape
+    func numberOfShapes(whiteboard: WhiteboardView) -> Int
+    func shapeAt(whiteboard: WhiteboardView, index: Int) -> Shape
+    func hasNewShape(whiteboard: WhiteboardView, shape: Shape)
 }
 
 class WhiteboardView: UIView {
 
+    var lineWidth: Double
+    var strokeColor: UIColor
+
     var delegate: WhiteboardViewDelegate?
 
+    private var currentShape: Shape?
+
+    override init(frame: CGRect) {
+        self.lineWidth = 4
+        self.strokeColor = UIColor.darkGrayColor()
+        super.init(frame: frame)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        self.lineWidth = 4
+        self.strokeColor = UIColor.darkGrayColor()
+        super.init(coder: aDecoder)
+    }
+
     func drawShapes() {
-        if let count = self.delegate?.numberOfShapes() {
+        if let count = self.delegate?.numberOfShapes(self) {
             for i in 0 ..< count {
-                if let shape = self.delegate?.shapeAt(i) {
+                if let shape = self.delegate?.shapeAt(self, index: i) {
                     self.drawShape(shape)
                 }
             }
@@ -43,5 +61,36 @@ class WhiteboardView: UIView {
 
     override func drawRect(rect: CGRect) {
         self.drawShapes()
+    }
+
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        let touch = touches.anyObject() as UITouch?
+        let location: CGPoint! = touch?.locationInView(self)
+
+        self.currentShape = Shape(
+            points: [location],
+            width: self.lineWidth,
+            strokeColor: self.strokeColor.CGColor
+        )
+    }
+
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        let touch = touches.anyObject() as UITouch?
+        let location: CGPoint! = touch?.locationInView(self)
+
+        // TODO: we may want to skip some location
+        self.currentShape?.points.append(location)
+    }
+
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        let touch = touches.anyObject() as UITouch?
+        let location: CGPoint! = touch?.locationInView(self)
+
+        self.delegate?.hasNewShape(self, shape: self.currentShape!)
+        self.currentShape = nil
+    }
+
+    override func touchesCancelled(touches: NSSet!, withEvent event: UIEvent!) {
+        self.currentShape = nil
     }
 }
