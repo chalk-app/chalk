9d0f51688096d086c4e4a96841f967c88f2920f1
Alex Ignatenko <alex@getbase.com>
2014-11-15 11:35:53 +0100

    Initial commit

---
+++ Chalk/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Chalk
+//
+//  Created by Alexander Ignatenko on 15/11/14.
+//  Copyright (c) 2014 Alexander Ignatenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
